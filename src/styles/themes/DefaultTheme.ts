35be87ce996a7a497c525e3f269866c41300444f
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-01-25 12:52:57 -0300

    fix: fixed theme types

--- src/styles/themes/DefaultTheme.ts
+++ src/styles/themes/DefaultTheme.ts
@@ -2,9 +2,6 @@ export const DefaultTheme = {
   white: "#FFF",
   background: "#FAFAFA",
   pink: "#FFB6C1",
-  "light-pink": "#FFB6C1",
   "gray-100": "#D9D9D9",
   "gray-200": "#2D2D2D",
 };
-
-// #DA57A3
