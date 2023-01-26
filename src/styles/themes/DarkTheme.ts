5afbd4943c91fd501df9deb308959679050eeb6f
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-01-26 18:52:20 -0300

    styles: changed colors of dark theme

--- src/styles/themes/DarkTheme.ts
+++ src/styles/themes/DarkTheme.ts
@@ -4,6 +4,6 @@ export const DarkTheme = {
   headerbackground: "#141414",
   background: "#0c0c0c",
   pink: "#FEB0F7",
-  "gray-100": "#D9D9D9",
-  "gray-200": "#D9D9D9",
+  "gray-100": "#FFF",
+  "gray-200": "#FFF",
 };
