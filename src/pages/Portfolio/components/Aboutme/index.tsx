a89cd68e51fdb0be4087a282367dac10d84e1007
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-01-26 18:57:05 -0300

    fix: grammar mistake

--- src/pages/Portfolio/components/Aboutme/index.tsx
+++ src/pages/Portfolio/components/Aboutme/index.tsx
@@ -12,7 +12,7 @@ export function Aboutme() {
 
         <p>
           Sou apaixonado por tecnologia desde pequeno, e minha profissão dos
-          sonhos, pelo incrível que pareça, sempre foi ser programador.
+          sonhos, por incrível que pareça, sempre foi ser programador.
           Atualmente atuo como front-end, mas também tenho interesse em me
           aventurar no back-end. A cada dia que passa o meu amor pela tecnologia
           vai aumentando mais e mais, e programar sempre foi e sempre vai ser
