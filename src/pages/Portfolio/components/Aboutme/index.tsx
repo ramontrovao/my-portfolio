37f05a409f0425365707bcc4a69445d1f032db5e
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-02-23 15:08:33 -0300

    feat: changed aboutme

--- src/pages/Portfolio/components/Aboutme/index.tsx
+++ src/pages/Portfolio/components/Aboutme/index.tsx
@@ -11,14 +11,14 @@ export function Aboutme() {
         <div></div>
 
         <p>
-          Olá, seja bem vindo ao meu portfólio! Meu nome é Ramon, tenho 15 anos
-          e programo há quase um ano! Sou apaixonado por Typescript e utilizo
-          React e NextJS no meu dia a dia. Atualmente sou desenvolvedor
-          front-end, porém tenho interesse em migrar para área mobile no futuro.
-          Sou apaixonado por tecnolgia desde os meus 8 anos, mas só fiz meu
-          primeiro curso no ínicio do ano passado. Completei o curso Fullstack
-          da Onebitcode e estou quase terminando o curso Ignite (avançado) da
-          Rocketseat na área de React! :)
+          Olá, seja bem vindo ao meu portfólio! Meu nome é Ramon, e eu programo
+          há quase um ano. Sou apaixonado por Typescript e utilizo React e
+          NextJS no meu dia a dia. Atualmente sou desenvolvedor front-end, porém
+          tenho interesse em migrar para área mobile no futuro. Sou apaixonado
+          por tecnolgia desde os meus 8 anos, mas só fiz meu primeiro curso no
+          ínicio do ano passado. Completei o curso Fullstack da Onebitcode e
+          estou quase terminando o curso Ignite (avançado) da Rocketseat na área
+          de React! :)
         </p>
       </div>
     </AboutmeContainer>
